78a794c60f035e2fd3fc15689300fcb152ce5190
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2022-05-06 13:10:35 -0700

    feat:express server added

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"views")));
+
+app.get("/",(req,res)=>{
+	res.sendFile(path.join(__dirname,"views/index.html"));
+});
+
+app.use("*",(req,res)=>{
+	res.status(404).send(path.join(__dirname,"views/404.html"));
+})
+
+app.listen(2200,()=>"Ready on port 2200");
